40e13bdaafafaef3c957e576502a7189b489a4de
rahull0328 <rmmehta2004@gmail.com>
2025-06-29 18:22:08 +0530

    changes to user and tale APIs

--- routes/tale.route.js
+++ routes/tale.route.js
@@ -1,9 +1,10 @@
-import { addTale } from "../controllers/tale.controller.js";
+import { addTale, getAllTales } from "../controllers/tale.controller.js";
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js"
 
 const router = express.Router()
 
 router.route("/addTale").post(isAuthenticated, addTale)
+router.route("/getAllTales").get(isAuthenticated, getAllTales)
 
 export default router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
-import express from "express"
-import { register, login } from "../controllers/user.controller.js"
+import express from "express";
+import { register, login, logout } from "../controllers/user.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
 router.route("/register").post(register);
-router.route("/login").get(login)
+router.route("/login").get(login);
+router.route("/logout").get(logout);
 
 export default router;

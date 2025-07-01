00c677e19970ee37333a4e3c0f78977af1b9f50d
rahull0328 <rmmehta2004@gmail.com>
2025-07-01 18:18:22 +0530

    added search API for tales

--- routes/tale.route.js
+++ routes/tale.route.js
@@ -1,14 +1,18 @@
-import { addTale, getAllTales, editTale, deleteTale, addToFavourites } from "../controllers/tale.controller.js";
+import { addTale, getAllTales, editTale, deleteTale, addToFavourites, searchTale } from "../controllers/tale.controller.js";
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
+//CRUD
 router.route("/addTale").post(isAuthenticated, singleUpload, addTale);
 router.route("/getAllTales").get(isAuthenticated, getAllTales);
 router.route("/editTale/:id").put(isAuthenticated, editTale)
 router.route("/deleteTale/:id").delete(isAuthenticated, deleteTale)
+
+//other functionalities
 router.route("/addToFavourites/:id").put(isAuthenticated, addToFavourites)
+router.route("/searchTale").get(isAuthenticated, searchTale)
 
 export default router;
